be77e07a5be51c40306df96e15f5aa99c263fd6e
Nilesh Narkhede <99078399+Nilesnarkhede@users.noreply.github.com>
2026-04-13 18:11:31 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,16 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$conn = new mysqli("localhost","root","Nilesh@123456","mini_chat");
+// environment variables
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = getenv("DB_PORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("DB Error: " . $conn->connect_error);
 }
+?>
